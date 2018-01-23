6ff3ba6662976f77673bdc960486a423ef06a4be
amazumdar <abhijeet.mazumdar@oath.com>
2018-01-23 12:49:18 -0500

    Initial project structure

---
+++ src/main/java/com/oath/hackday/iotexplore/IotExploreApplication.java
@@ -0,0 +1,12 @@
+package com.oath.hackday.iotexplore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotExploreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotExploreApplication.class, args);
+	}
+}
---
+++ src/test/java/com/oath/hackday/iotexplore/IotExploreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.oath.hackday.iotexplore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IotExploreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
